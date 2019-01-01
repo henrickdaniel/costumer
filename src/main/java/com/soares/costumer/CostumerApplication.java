a3dada4931eafca158dd1740a1d5b3ae35643dda
Henrick Daniel Soares <henrickdaniel@gmail.com>
2019-01-01 16:18:56 -0200

    Reading Configuration from the Config Server using the Config Client

--- src/main/java/com/soares/costumer/CostumerApplication.java
+++ src/main/java/com/soares/costumer/CostumerApplication.java
@@ -2,8 +2,10 @@ package com.soares.costumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.soares.costumer")
 public class CostumerApplication {
 
 	public static void main(String[] args) {
