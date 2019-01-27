2ab98ace64751d31e511703c186bcceeb76be2dd
Henrick Daniel Soares <henrickdaniel@gmail.com>
2019-01-27 11:05:19 -0200

    adjusting pom.xml add dependency for eureka

--- src/main/java/com/soares/costumer/CostumerApplication.java
+++ src/main/java/com/soares/costumer/CostumerApplication.java
@@ -2,8 +2,10 @@ package com.soares.costumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.soares.costumer")
 public class CostumerApplication {
--- src/main/java/com/soares/costumer/controller/CostumerController.java
+++ src/main/java/com/soares/costumer/controller/CostumerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soares.costumer.model.Costumer;
 import com.soares.costumer.repository.CostumerRepository;
 
+
 @RestController
 @RequestMapping("/api")
 public class CostumerController {
